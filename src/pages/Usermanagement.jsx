12afa0eb6a13000778318be4027f9a6c15ce428a
nigam <nigammishra826@gmail.com>
2024-04-06 00:24:56 +0530

    usermanagement page 403 error code completed

--- src/pages/Usermanagement.jsx
+++ src/pages/Usermanagement.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Usermanagement = () => {
   return (
-    <div>Usermanagement</div>
+    <div>
+      <>
+     <section className="page-not-found">
+  <img src="https://image.freepik.com/free-vector/403-error-forbidden-with-police-concept-illustration_114360-1884.jpg" alt />
+  <h1>Page NOt Found</h1>
+  <p>
+    sorry can't find the page you are looking for click
+    Below to come back to home page.
+  </p>
+  <Link to="/Welcomepage"><button type="button">Back</button></Link>
+</section>
+
+      </>
+    </div>
   )
 }
 
