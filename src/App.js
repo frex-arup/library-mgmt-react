4aa7593396795bbaa8961f5cac1bd54ee05c9521
Arupananda Pal <arupananda.pal@bipros.com>
2024-04-12 12:29:44 +0530

    login logout initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
+import { Navigate, Outlet, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './component/Header';
 import Footer from './component/Footer';
+import Header from './component/Header';
 import Bookmanagement from './pages/Bookmanagement';
 import Createnewbook from './pages/Createnewbook';
-import Usermanagement from './pages/Usermanagement';
-import Welcomepage from './pages/Welcomepage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Usermanagement from './pages/Usermanagement';
+import Welcomepage from './pages/Welcomepage';
+
+
+const PrivateRoutes = () => {
+  const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+  return (
+    isLoggedIn ? <Outlet /> : <Navigate to='/login' />
+  )
+}
+
 function App() {
+  const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+
   return (
     <Router>
-      <Header/> 
-      <Routes> 
-        <Route exact path="/" element={<Login/>}/>
-        <Route exact path="/Register" element={<Register/>}/>
-        <Route exact path="/Bookmanagement" element={<Bookmanagement/>}/>
-        <Route exact path="/Createnewbook" element={<Createnewbook/>}/>
-        <Route exact path="/update-book/:id" element={<Createnewbook/>}/>
-        <Route exact path="/Usermanagement" element={<Usermanagement/>}/>
-        <Route exact path="/library-mgmt-react" element={<Welcomepage/>}/>      
-      </Routes> 
-      <Footer/>
+      {isLoggedIn && <Header />}
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Bookmanagement" element={<Bookmanagement />} />
+          <Route path="/Createnewbook" element={<Createnewbook />} />
+          <Route path="/update-book/:id" element={<Createnewbook />} />
+          <Route path="/Usermanagement" element={<Usermanagement />} />
+          <Route path="/library-mgmt-react" element={<Welcomepage />} />
+          <Route path="/" element={<Welcomepage />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      {isLoggedIn && <Footer />}
     </Router>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import Loginimage from '../Images/loginpage.png';
+import { Link, useNavigate } from 'react-router-dom';
 import Loginimage1 from '../Images/login.png';
-import { Link } from 'react-router-dom';
+import Loginimage from '../Images/loginpage.png';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
 
   const handleLogin = () => {
     console.log('Logging in with:', { username, password });
@@ -15,46 +17,58 @@ function Login() {
     console.log('Registering with:', { username, password });
   };
 
-  return (
-<>
-<h1>Library Management App</h1>
-    <div className="container-fluid">
-      <div className="row" style={{gap:'50px'}}>
-        <div className="col-md-6 ">
-        <img src={Loginimage} className='login-image' alt="Login" />
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === 'admin' && password === 'admin') {
+      sessionStorage.setItem('isLoggedIn', 'true');
+      // Redirect to the protected route
+      navigate('/library-mgmt-react');
+    } else {
+      // Show an error message
+      alert('Invalid username or password');
+    }
+  };
 
-        </div>
-        <div className="col-md-6 login-form">
-          <form>
-          <img src={Loginimage1} className='login-image1' alt="Login" />
-            <div className="mb-3">
-              <label htmlFor="username" className="form-label">Username</label>
-              <input
-                type="text"
-                className="form-control"
-                id="username"
-                placeholder="Enter username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <Link to="/"><button type="button" className="btn btn-primary" onClick={handleLogin}>Login</button></Link>
-            <Link to="/Register"><button type="button" className="btn btn-secondary ms-2" onClick={handleRegister}>Register</button></Link>
-          </form>
+  return (
+    <>
+      <h1>Library Management App</h1>
+      <div className="container-fluid">
+        <div className="row" style={{ gap: '50px' }}>
+          <div className="col-md-6 ">
+            <img src={Loginimage} className='login-image' alt="Login" />
+
+          </div>
+          <div className="col-md-6 login-form">
+            <form onSubmit={handleSubmit}>
+              <img src={Loginimage1} className='login-image1' alt="Login" />
+              <div className="mb-3">
+                <label htmlFor="username" className="form-label">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  placeholder="Enter username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary" onClick={handleLogin}>Login</button>
+              <Link to="/Register"><button type="button" className="btn btn-secondary ms-2" onClick={handleRegister}>Register</button></Link>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
